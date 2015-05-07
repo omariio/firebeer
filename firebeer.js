3c38dff1e66f23f748ac4c2f512e0f597f462942
Tyler Smith <tylsmith@gmail.com>
2015-05-07 13:41:15 -0700

    init 2

---
+++ firebeer.js
@@ -0,0 +1,29 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.feed.helpers({
+    docs: function () {
+      return Docs.find();
+    },
+    string: function () {
+      return JSON.stringify(this);
+    }
+  });
+
+  Template.jsonInterpreter.events({
+    'click button': function () {
+      var json = $("#textarea-xml-input").val();
+      var obj = jQuery.parseJSON(json);
+      Docs.insert(obj);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+Docs = new Meteor.Collection("docs");
